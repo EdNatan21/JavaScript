a49471a54d539c5b296e99a5c439828c4fd21668
EdNatan21 <90695383+EdNatan21@users.noreply.github.com>
2023-03-13 17:28:14 -0300

    aula014

---
+++ Aula14Ex/Ex016/script.js
@@ -0,0 +1,12 @@
+function contagem(){
+    var come = document.querySelector('input#ini')
+    var end = document.querySelector('input#fim')
+    var pulo = document.querySelector('input#pas')
+    var local = document.querySelector('div#res')
+    var cont
+
+    for(come = 0; end < 50; cont = cont + pulo){
+        alert(`cont`)
+    }
+    
+} 
